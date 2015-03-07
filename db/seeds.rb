3ba216e8dae3903ded67d4a4f906dc54d4c00f7e
Yumekia <yumekiaw@yahoo.com>
2015-03-07 22:42:04 +0000

    Complete Seed Data assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,15 @@
    )
  end
  posts = Post.all
- 
+
+ 30.times do
+ 	Post.create!(
+ 		title: Faker::Lorem.characters,
+ 		body: Faker::Lorem.words
+ 	)
+end
+posts = Post.all
+
  # Create Comments
  100.times do
    Comment.create!(
@@ -16,7 +24,14 @@
      body: Faker::Lorem.paragraph
    )
  end
- 
+
+ 60.times do
+ 	Comment.create!(
+      post: posts.sample,
+      body: Faker::Lorem.words
+ 	)
+ end
+
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
