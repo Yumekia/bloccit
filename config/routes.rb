ceeb557bc71b93810497e72a12d4f4228b512647
Yumekia <yumekiaw@yahoo.com>
2015-04-18 16:12:22 +0000

    Destroy Checkpoint Completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     resources :users, only: [:update]
   
   resources :topics do
-    resources :posts, except: [:index] do
-     resources :comments, only: [:create]
+    resources :posts, except: [:index] do 
+      resources :comments, only: [:create, :destroy]
     end
    end
   
