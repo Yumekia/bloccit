0bf749f7385742540f45230c1e8e4256547919b9
Yumekia <yumekiaw@yahoo.com>
2015-05-02 15:34:07 +0000

    Complete assignment private profiles

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
  describe "Visiting profiles" do
  
    include TestFactories
+   include Warden::Test::Helpers
+   Warden.test_mode!
  
    before do
-     @user = authenticated_user
+       @user = authenticated_user
         @post = associated_post(user: @user)
         @comment = Comment.create(user: @user, post: @post, body: "A Comment")
         allow(@comment).to receive(:send_favorite_emails)
@@ -23,6 +25,20 @@ require 'rails_helper'
        expect( page ).to have_content(@comment.body)
      
      end
- 
    end
+ 
+   describe "signed in as admin" do
++
++    before { login_as authenticated_user(role: 'admin'), scope: :user }
++
++    it "shows profile" do
++      visit user_path(@user)
++      expect(current_path).to eq(user_path(@user))
++
++      expect( page ).to have_content(@user.name)
++      expect( page ).to have_content(@post.title)
++      expect( page ).to have_content(@comment.body)
++    end
++  end
+ 
  end
